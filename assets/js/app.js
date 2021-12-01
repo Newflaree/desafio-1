2b9ff7984e7677c1397ad57f03943605b60c38dd
Camilo López <camiloj.lopez79@gmail.com>
2021-12-01 20:43:03 -0300

    Se crea la estructura del proyecto

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+function desafio1() {
+  alert( 'Hola 1' );
+}
+
+function desafio2() {
+  alert( 'Hola 2' );
+}
+
+function desafio3() {
+  alert( 'Hola 3' );
+}
+
+function desafio4() {
+  alert( 'Hola 4' );
+}
+
+function desafio5() {
+  alert( 'Hola 5' );
+}
+
+
+
